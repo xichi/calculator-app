4727df078fb368012fc3d6272b3d581595023429
包宇豪 <1722974163@qq.com>
2020-10-14 01:35:29 +0800

    UI design in iOS style

--- app/views/UnitConversion/UnitKind/Weight.js
+++ app/views/UnitConversion/UnitKind/Weight.js
@@ -1,244 +1,281 @@
-import React ,{useState} from 'react';
-import { View,Text,StyleSheet,TextInput,TouchableOpacity, Keyboard } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  ScrollView,
+  Alert,
+} from 'react-native';
 
-const Length =() =>{
-    const [mm,setmm]=useState('');
-    const [cm,setcm]=useState('');
-    const [dm,setdm]=useState('');
-    const [m,setm]=useState('');
-    const [km,setkm]=useState('');
+const Weight = () => {
+  const [mm, setmm] = useState('');
+  const [cm, setcm] = useState('');
+  const [dm, setdm] = useState('');
+  const [m, setm] = useState('');
+  const [km, setkm] = useState('');
+  const [foot, setfoot] = useState('');
+  const [inch, setinch] = useState('');
+  const [flagmm, setflagmm] = useState(true);
+  const [flagdm, setflagdm] = useState(true);
+  const [flagm, setflagm] = useState(true);
+  const [flagkm, setflagkm] = useState(true);
+  const [flagfoot, setflagfoot] = useState(true);
+  const [flaginch, setflaginch] = useState(true);
 
-    function Transform(){
-        if(mm!=''){
-            let indexmm = parseFloat(mm);
-            let indexcm = indexmm/10;
-            let indexdm = indexmm/100;
-            let indexm = indexmm/1000;
-            let indexkm = indexmm/1000000;
-            setcm(indexcm);
-            setdm(indexdm);
-            setm(indexm);
-            setkm(indexkm);
-        }
-        if(cm!=''){
-            let indexcm = parseFloat(cm);
-            let indexmm = indexcm*10;
-            let indexdm = indexcm/10;
-            let indexm = indexcm/100;
-            let indexkm = indexcm/100000;
-            setmm(indexmm);
-            setdm(indexdm);
-            setm(indexm);
-            setkm(indexkm);
-        }
-        if(dm!=''){
-            let indexdm = parseFloat(dm);
-            let indexmm = indexdm*100;
-            let indexcm = indexdm*10;
-            let indexm = indexdm/10;
-            let indexkm = indexdm/10000;
-            setcm(indexcm);
-            setmm(indexmm);
-            setm(indexm);
-            setkm(indexkm);
-        }
-        if(m!=''){
-            let indexm = parseFloat(m);
-            let indexcm = indexm*100;
-            let indexdm = indexm*10;
-            let indexmm = indexm*1000;
-            let indexkm = indexm/1000;
-            setcm(indexcm);
-            setdm(indexdm);
-            setmm(indexmm);
-            setkm(indexkm);
-        }
-        if(km!=''){
-            let indexkm = parseFloat(km);
-            let indexcm = indexkm*100000;
-            let indexdm = indexkm*10000;
-            let indexm = indexkm*1000;
-            let indexmm = indexkm*1000000;
-            setcm(indexcm);
-            setdm(indexdm);
-            setm(indexm);
-            setmm(indexmm);
-        }
+  function duserqq(indexm) {
+    let indexkm = (indexm / 1000).toFixed(2);
+    let indexdm = (indexm * 10).toFixed(2);
+    let indexcm = (indexm * 100).toFixed(2);
+    let indexmm = (indexm * 1000).toFixed(2);
+    let indexfoot = (indexm * 3.2808399).toFixed(2);
+    let indexinch = (indexm * 39.3700787).toFixed(2);
+    setcm(indexcm);
+    setdm(indexdm);
+    setmm(indexmm);
+    setkm(indexkm);
+    setfoot(indexfoot);
+    setinch(indexinch);
+    setm(indexm);
+    alert('Designed by NJR10byh in China.\nAssembled in NJUPT');
+  }
+
+  function Transform() {
+    if (mm != '') {
+      let indexmm = parseFloat(mm);
+      indexm = (indexmm / 1000).toFixed(2);
+      //alert(indexm);
+      duserqq(indexm);
     }
-    function Clear(){
-        setmm('');
-        setcm('');
-        setdm('');
-        setm('');
-        setkm('');
+    if (cm != '') {
+      let indexcm = parseFloat(cm);
+      indexm = (indexcm / 100).toFixed(2);
+      duserqq(indexm);
     }
-        return(
-            <View style={styles.body}>
-            <View style={styles.row}>
-            <Text style={styles.text}>毫米:  {mm} mm</Text>
-                <TextInput
-                    placeholder="毫米" 
-                    keyboardType='number-pad'
-                    clearButtonMode="while-editing"
-                    clearTextOnFocus={true}
-                    returnKeyType="done"
-                    onChangeText={(text) =>{ 
-                        setmm(text);
-                        setcm('');
-                        setdm('');
-                        setm('');
-                        setkm('');
-                    }}
-                    style={styles.inputstyle}
-                />
-            </View>
-            <View style={styles.row}>
-            <Text style={styles.text}>厘米:  {cm} cm</Text>
-                <TextInput
-                    placeholder="厘米" 
-                    keyboardType='number-pad'
-                    clearButtonMode="while-editing"
-                    clearTextOnFocus={true}
-                    returnKeyType="done"
-                    onChangeText={(text) =>{ 
-                        setmm('');
-                        setcm(text);
-                        setdm('');
-                        setm('');
-                        setkm('');
-                    }}
-                    style={styles.inputstyle}
-                />
-            </View>
-            <View style={styles.row}>
-            <Text style={styles.text}>分米:  {dm} dm</Text>
-                <TextInput
-                    placeholder="分米" 
-                    keyboardType='number-pad'
-                    clearButtonMode="while-editing"
-                    clearTextOnFocus={true}
-                    returnKeyType="done"
-                    onChangeText={(text) =>{ 
-                        setmm('');
-                        setcm('');
-                        setdm(text);
-                        setm('');
-                        setkm('');
-                    }}
-                    style={styles.inputstyle}
-                />
-            </View>
-            <View style={styles.row}>
-            <Text style={styles.text}>     米:  {m} m</Text>
-                <TextInput
-                    placeholder="米" 
-                    keyboardType='number-pad'
-                    clearButtonMode="while-editing"
-                    clearTextOnFocus={true}
-                    returnKeyType="done"
-                    onChangeText={(text) =>{ 
-                        setmm('');
-                        setcm('');
-                        setdm('');
-                        setm(text);
-                        setkm('');
-                    }}
-                    style={styles.inputstyle}
-                />
-            </View>
-            <View style={styles.row}>
-            <Text style={styles.text}>千米:  {km} km</Text>
-                <TextInput
-                    placeholder="千米"
-                    placeholdertTextColor='red'
-                    keyboardType='number-pad'
-                    clearButtonMode="while-editing"
-                    clearTextOnFocus={true}
-                    returnKeyType="done"
-                    onChangeText={(text) =>{ 
-                        setmm('');
-                        setcm('');
-                        setdm('');
-                        setm('');
-                        setkm(text);
-                    }}
-                    style={styles.inputstyle}
-                />
-            </View>
-            <TouchableOpacity 
-                style={styles.btn1}
-                onPress={Transform}
+    if (dm != '') {
+      let indexdm = parseFloat(dm);
+      indexm = (indexdm / 10).toFixed(2);
+      duserqq(indexm);
+    }
+    if (m != '') {
+      indexm = parseFloat(m).toFixed(2);
+      duserqq(indexm);
+    }
+    if (km != '') {
+      let indexkm = parseFloat(km);
+      indexm = (indexkm * 1000).toFixed(2);
+      duserqq(indexm);
+    }
+    if (foot != '') {
+      let indexfoot = parseFloat(foot);
+      indexm = (indexfoot * 0.3048).toFixed();
+      duserqq(indexm);
+    }
+    if (inch != '') {
+      let indexinch = parseFloat(inch);
+      indexm = (indexinch * 0.0254).toFixed(2);
+      duserqq(indexm);
+    }
+  }
+  function Clear() {
+    setmm('');
+    setcm('');
+    setdm('');
+    setm('');
+    setkm('');
+  }
+  return (
+    <ScrollView>
+      <KeyboardAvoidingView behavior="position">
+        <View style={styles.body}>
+          <View style={styles.valuetop}>
+            <Text style={{ fontSize: 30, fontWeight: '500', marginTop: 3 }}>
+              数值:{' '}
+            </Text>
+            <TextInput style={styles.inputstyletop} placeholder="请输入数值" />
+          </View>
+          <View style={styles.units}>
+            <TouchableOpacity
+              onPress={() => {
+                setflagmm(!flagmm);
+              }}
+              style={[
+                styles.unitsview,
+                { backgroundColor: flagmm ? 'white' : 'blue' },
+              ]}
             >
-                <Text style={[styles.btnText,{color:'black'}]}>转换</Text>
+              <Text
+                style={[styles.text, { color: flagmm ? '#31A4F0' : 'white' }]}
+              >
+                毫米: {mm}{' '}
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity 
-                style={styles.btn2}
-                onPress={Clear}
+            <TouchableOpacity
+              onPress={() => {
+                setflagdm(!flagdm);
+              }}
+              style={[
+                styles.unitsview,
+                { backgroundColor: flagdm ? 'white' : 'blue' },
+              ]}
             >
-                <Text style={styles.btnText}>重置</Text>
+              <Text
+                style={[styles.text, { color: flagdm ? '#31A4F0' : 'white' }]}
+              >
+                分米: {dm} dm{' '}
+              </Text>
             </TouchableOpacity>
-            </View>
-        );
-}
+            <TouchableOpacity
+              onPress={() => {
+                setflagm(!flagm);
+              }}
+              style={[
+                styles.unitsview,
+                { backgroundColor: flagm ? 'white' : 'blue' },
+              ]}
+            >
+              <Text
+                style={[styles.text, { color: flagm ? '#31A4F0' : 'white' }]}
+              >
+                米: {m} m
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setflagkm(!flagkm);
+              }}
+              style={[
+                styles.unitsview,
+                { backgroundColor: flagkm ? 'white' : 'blue' },
+              ]}
+            >
+              <Text
+                style={[styles.text, { color: flagkm ? '#31A4F0' : 'white' }]}
+              >
+                千米: {km} km
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setflagfoot(!flagfoot);
+              }}
+              style={[
+                styles.unitsview,
+                { backgroundColor: flagfoot ? 'white' : '#3333ff' },
+              ]}
+            >
+              <Text
+                style={[styles.text, { color: flagfoot ? '#31A4F0' : 'white' }]}
+              >
+                英尺: {foot} foot
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setflaginch(!flaginch);
+              }}
+              style={[
+                styles.unitsview,
+                { backgroundColor: flaginch ? 'white' : 'blue' },
+              ]}
+            >
+              <Text
+                style={[styles.text, { color: flaginch ? '#31A4F0' : 'white' }]}
+              >
+                英寸: {inch} inch
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity style={styles.btn1} onPress={Transform}>
+            <Text style={[styles.btnText, { color: 'white' }]}>转换</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn2} onPress={Clear}>
+            <Text style={styles.btnText}>重置</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
+  );
+};
 
-const styles=StyleSheet.create({
-    body:{
-        padding:15,
-        height:'100%',
-        backgroundColor:'black',
-    },
-    row:{
-        flexDirection:'row',
-        height:40,
-        marginTop:20,
-    },
-    text:{
-        flex:1,
-        height:100,
-        marginTop:8,  
-        color:'#31A4F0',
-        fontSize:23,
-        fontWeight:'600',
-    },
-    inputstyle:{
-        width:160,
-        height:45,
-        padding:10,
-        fontSize:18,
-        borderColor:'orange',
-        borderWidth:0.8,
-        //borderBottomWidth:1,
-        borderRadius:10,
-        color:'#fff',
-        fontWeight:'500'
-    },
-    btn1:{
-        backgroundColor:'white',
-        padding:5,
-        width:80,
-        height:80,
-        borderRadius:50,
-        position:"absolute",
-        top:360,
-        left:80,
-    },
-    btn2:{
-        backgroundColor:'red',
-        padding:5,
-        width:80,
-        height:80,
-        borderRadius:50,
-        position:"absolute",
-        top:360,
-        left:260,
-    },
-    btnText:{
-        fontSize:25,
-        color:'white',
-        fontWeight:'bold',
-        textAlign:'center',
-        marginTop:23,
-    }
+const styles = StyleSheet.create({
+  body: {
+    padding: 15,
+    //For Dark Mode
+    //backgroundColor:'black',
+    backgroundColor: 'white',
+  },
+  valuetop: {
+    flexDirection: 'row',
+  },
+  units: {
+    flexDirection: 'column',
+    marginTop: 20,
+  },
+  unitsview: {
+    borderStyle: 'solid',
+    borderBottomColor: '#CCCCCC',
+    borderBottomWidth: 0.6,
+    paddingTop: 5,
+    paddingBottom: 10,
+    paddingLeft: 5,
+    marginTop: 5,
+    borderRadius: 15,
+  },
+  text: {
+    height: 30,
+    marginTop: 8,
+    fontSize: 23,
+    color: '#31A4F0',
+    fontWeight: '600',
+  },
+  inputstyletop: {
+    width: 285,
+    height: 45,
+    padding: 10,
+    marginLeft: 25,
+    fontSize: 18,
+    backgroundColor: '#EEEEEE',
+    //For Dark Mode
+    //borderColor:'orange',
+    //borderWidth:0.8,
+    borderRadius: 10,
+    //For Dark Mode
+    //color:'#fff',
+    color: '#111',
+    fontWeight: '500',
+  },
+  btn1: {
+    //For Dark Mode
+    //backgroundColor:'white',
+    backgroundColor: 'black',
+    padding: 5,
+    width: 150,
+    height: 80,
+    borderRadius: 20,
+    position: 'absolute',
+    top: 480,
+    left: 40,
+  },
+  btn2: {
+    backgroundColor: 'red',
+    padding: 5,
+    width: 150,
+    height: 80,
+    borderRadius: 20,
+    position: 'absolute',
+    top: 480,
+    left: 220,
+  },
+  btnText: {
+    fontSize: 25,
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 23,
+  },
 });
 
-export default Length;
+export default Weight;
